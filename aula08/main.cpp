fcdd861a41914d6685a960d6970587e2d23d2916
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2019-08-27 10:36:05 -0300

    Stack start (2)

---
+++ aula08/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+#define DEBUG 0 
+
+class Stack {
+
+private:
+	#define MAX 8
+	int top = 0;
+	int data[MAX]; // 1..7
+	void dump();
+
+public:
+	int empty();
+	int full();
+	int pop();
+	void push(int x);
+};
+// Stack begin
+int Stack::empty() {
+	if (top == 0)
+		return 1;
+	else return 0;
+}
+
+int Stack::full() {
+	if (top == MAX - 1)
+		return 1;
+	else return 0;
+}
+
+void Stack::push(int x) {
+	if (full()) {
+		// error
+		cerr << "Pilha cheia!" << endl;
+		return;
+	} else {
+		top = top + 1;
+		data[top] = x;
+		if (DEBUG)
+			dump();
+	}
+}
+
+int Stack::pop() {
+	if (empty()) {
+		// error
+		cerr << "Pilha vazia!" << endl;
+		return -1;
+	} else {
+		top = top - 1;
+		if (DEBUG)
+			dump();
+		return data[top + 1];
+	}
+}
+
+void Stack::dump() {
+	cout << "top: " << top << endl;
+	// mostrar conteúdo da pilha
+	cout << "{ ";
+	for (int i = 1 ; i <= top ; i++) {
+		cout << data[i] << " ";
+	}
+	cout << "}";
+	cout << endl;
+}
+// Stack end
+
+
+int main() {
+	cout << "Pilha com classes" << endl;
+
+	return 0;
+}
